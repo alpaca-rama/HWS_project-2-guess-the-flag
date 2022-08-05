c62f9dd720502be0aec21ba9af617f445aae307d
Luca <luca@weve-got-it.com>
2022-08-05 19:44:06 +0200

    Initial Commit

---
+++ project-2-guess-the-flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-2-guess-the-flag
+//
+//  Created by Luca Capriati on 2022/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
