5b500ab2fb251bfe00ea162cc2d2eeb1976bc282
Luca <luca@weve-got-it.com>
2022-08-06 16:03:04 +0200

    Project 2 - Part 2 : Adding the playerScore variable to keep track and display the score of the user. Change the background to a linear gradient. The changing of font on the main screen. The shaping of the flags to a capsule shape, with addition to a shadow.

--- project-2-guess-the-flag/ContentView.swift
+++ project-2-guess-the-flag/ContentView.swift
@@ -12,18 +12,21 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var playerScore = 0
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 20) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 
                 ForEach(0..<3) {number in
@@ -32,6 +35,8 @@ struct ContentView: View {
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
                     }
                 }
             }
@@ -39,13 +44,14 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(playerScore)")
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            playerScore += 1
         } else {
             scoreTitle = "Wrong"
         }
