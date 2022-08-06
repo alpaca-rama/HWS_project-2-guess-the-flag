c668d711f1f2baadc5c8d40bc6265da23f14af99
Luca <luca@weve-got-it.com>
2022-08-06 13:03:52 +0200

    Project 2 - Part 2 - Added image assets of the flags. Created country array to store countries. Utilised a randomiser to pick an option (to be changed in the future). The country to be guessed along with flag options are displayed

--- project-2-guess-the-flag/ContentView.swift
+++ project-2-guess-the-flag/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany",
+                     "Ireland", "Italy", "Nigeria",
+                     "Poland", "Russia", "Spain",
+                     "Uk", "Us"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) {number in
+                    Button {
+                        // Flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
     }
 }
 
