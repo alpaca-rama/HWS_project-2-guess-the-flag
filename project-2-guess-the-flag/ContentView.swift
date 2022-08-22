095c325a92c6f8ce4860b601a56a3665e81932ea
Luca <luca@weve-got-it.com>
2022-08-22 13:00:57 +0200

    Revised: Going over all the projects to fix everything and re do challenges. Project 2 challenges + bonus challenge.

--- project-2-guess-the-flag/ContentView.swift
+++ project-2-guess-the-flag/ContentView.swift
@@ -1,91 +1,80 @@
 //
 //  ContentView.swift
-//  project-2-guess-the-flag
+//  GuessTheFlag
 //
-//  Created by Luca Capriati on 2022/08/05.
+//  Created by Paul Hudson on 20/10/2021.
 //
-
+// Project 2 - Challange 1: Add an @State property to store the user’s score, modify it when they get an answer right or wrong,
+//  then display it in the alert and in the score label.
+// Project 2 - Challange 2: When someone chooses the wrong flag, tell them their mistake in your alert message –
+//  something like “Wrong! That’s the flag of France,” for example.
+// Project 2 - Challange 3: Make the game show only 8 questions, at which point they see a final alert judging their score
+//  and can restart the game.
+// Project 2 - Bonus Challange - Removing repeats
 import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    // Project 2 - Bonus Challange
+    @State private var countries = allCountries.shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    @State private var playerScore = 0
-    @State private var question = 1
-    @State private var questionsAlert = false
-    // Project 6 - Challange 1
-    @State private var isCorrect = false
-    @State private var selectedNumber = 0
-    // Project 6 - Challange 2 & 3
-    @State private var isIncorrect = false
-    
-    private var numberOfQuestions = 8
-    
-    struct ImageView: View {
-        var image: String
-        
-        var body: some View {
-            Image(image)
-                .renderingMode(.original)
-                .clipShape(Capsule())
-                .shadow(radius: 5)
-        }
-    }
-    
+    // Project 2 - Challange 1
+    @State private var score = 0
+    // Project 2 - Challange 3
+    @State private var questionCounter = 1
+    @State private var showingResults = false
+    // Project 2 - Bonus Challange
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+
     var body: some View {
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)], center: .top, startRadius: 200, endRadius: 600)
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
+
             VStack {
                 Spacer()
-                Text("Guess the flag")
+
+                Text("Guess the Flag")
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
-                Spacer()
-                Text("Question \(question) of \(numberOfQuestions)")
-                    .foregroundStyle(.primary)
-                    .font(.title.bold())
+
                 VStack(spacing: 15) {
                     VStack {
                         Text("Tap the flag of")
                             .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))
+
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-                    
-                    ForEach(0..<3) {number in
+
+                    ForEach(0..<3) { number in
                         Button {
-                            withAnimation {
-                                flagTapped(number)
-                            }
+                            flagTapped(number)
                         } label: {
-                            ImageView(image: countries[number])
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
                         }
-                        // Project 6 - Challange 1
-                        .rotation3DEffect(.degrees(isCorrect && selectedNumber == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
-                        // Project 6 - Challange 2
-                        .opacity(isIncorrect && selectedNumber != number ? 0.25 : 1)
-                        // Project 6 - Challange 3
-                        .scaleEffect(isIncorrect && selectedNumber != number ? 0.5 : 1)
                     }
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
                 .background(.regularMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
-                
+
                 Spacer()
                 Spacer()
-                
-//                Text("Score: \(playerScore)")
-//                    .foregroundColor(.white)
-//                    .font(.title.bold())
-                
+
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+
                 Spacer()
             }
             .padding()
@@ -93,50 +82,59 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Question \(question) of \(numberOfQuestions)")
+            Text("Your score is \(score)")
         }
-        .alert("You scored: \(playerScore)", isPresented: $questionsAlert) {
-            Button("no") {
-                
-            }
-            Button("YES", action: reset)
+        // Project 2 - Challange 3
+        .alert("Game over!", isPresented: $showingResults) {
+            Button("Start Again", action: newGame)
         } message: {
-            Text("Do you want to play again?")
+            Text("Your final score was \(score).")
         }
     }
-    
+
     func flagTapped(_ number: Int) {
-        selectedNumber = number
         if number == correctAnswer {
-            scoreTitle = "Correct!"
-            playerScore += 1
-            isCorrect = true
-            isIncorrect = true
+            scoreTitle = "Correct"
+            // Project 2 - Challange 1
+            score += 1
         } else {
-            scoreTitle = """
-                            Wrong!
-                            That's the flag of \(countries[number])
-                        """
+            // Project 2 - Challange 1
+            let needsThe = ["UK", "US"]
+            let theirAnswer = countries[number]
+            
+            if needsThe.contains(theirAnswer) {
+                scoreTitle = "Wrong! That's the flag of the \(countries[number])."
+            } else {
+                scoreTitle = "Wrong! That's the flag of \(countries[number])."
+            }
+            // Project 2 - Challange 1
+            if score > 0 {
+                score -= 1
+            }
         }
-    
-        showingScore = true
+        
+        // Project 2 - Challange 3
+        if questionCounter == 8 {
+            showingResults = true
+        } else {
+            showingScore = true
+        }
     }
-    
+
     func askQuestion() {
-        if question < numberOfQuestions {
-            countries.shuffle()
-            correctAnswer = Int.random(in: 0...2)
-            question += 1
-            isCorrect = false
-            isIncorrect = false
-        } else {
-            questionsAlert = true
-        }
+        // Project 2 - Bonus Challange
+        countries.remove(at: correctAnswer)
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        // Project 2 - Challange 3
+        questionCounter += 1
     }
     
-    func reset() {
-        question = 0
-        playerScore = 0
+    func newGame() {
+        questionCounter = 0
+        score = 0
+        // Project 2 - Bonus Challange
+        countries = Self.allCountries
         askQuestion()
     }
 }
